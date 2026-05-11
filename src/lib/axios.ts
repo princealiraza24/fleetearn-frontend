e0eee3c03cc5844edb58e6a3b6c7825dff40f8e6
ALI RAZA <hananalikhan5@gmail.com>
2026-05-10 19:53:33 -0700

    revert credentials

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from '@/stores/authStore'
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { 'Content-Type': 'application/json' },
-  withCredentials: false,
+  withCredentials: true,
 })
 
 // Attach token to every request
